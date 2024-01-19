9543d34e505850aaed0d1de52fbb5272163255a2
Fakhri Ramdani <fakhriramdani4@gmail.com>
2024-01-19 10:15:45 +0700

    init docker

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [name, setName] = useState("")
   return (
-    <div>hello universal studio japan</div>
+    <div>hello world </div>
   )
 }
