e6bc588e233300e38a91ad0cb0efeb4fa0431683
Fakhri Ramdani <fakhriramdani4@gmail.com>
2024-01-26 09:09:32 +0700

    add cv

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
       <AboutPage name={data.aboutPage.name} about={data.aboutPage.about} />
       <div className="min-h-[100vh] md:min-h-[110vh] min-w-full bg-gray-950">
         <div className="flex flex-col items-center pt-32 h-[30vh]">
-          <a href="#contact" className="bg-transparent  hover:bg-gray-500 text-gray-400 font-bold hover:text-gray-950 py-3 px-5 border border-gray-500 hover:border-transparent rounded" >
+          <a href="/cv.pdf" download="fakhri ramdani-cv" className="bg-transparent  hover:bg-gray-500 text-gray-400 font-bold hover:text-gray-950 py-3 px-5 border border-gray-500 hover:border-transparent rounded" >
             DOWNLOAD CV
           </a>
         </div>
